d1dd274c5cb4c53dbb5b309ad9828ddf34e29976
nataliMircheva <nataliadimova@abv.bg>
2021-11-30 14:56:48 +0200

    Initial commit

---
+++ src/main/java/com/aacademy/git_demoo/GitDemooApplication.java
@@ -0,0 +1,13 @@
+package com.aacademy.git_demoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDemooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitDemooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aacademy/git_demoo/GitDemooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aacademy.git_demoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
